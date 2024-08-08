025771a9d0aab98e3febb0f3904c02bd4ed265c9
Aman Verma <aka.amanverma@gmail.com>
2024-08-08 13:26:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -8,72 +8,52 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
-    // Reverse the linked list pointed by 'head', update 'prev' to point to the new head.
-    // Returns the length of the reversed list.
-    int reverse(ListNode* head, ListNode*& prev) {
-        ListNode* ptr = head;
-        ListNode* next = NULL;
-        int len = 0;
-        while (ptr) {
-            next = ptr->next;
-            ptr->next = prev;
-            prev = ptr;
-            ptr = next;
-            len++;
+    // Helper function to reverse a linked list
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
         }
-        return len;
+        return prev;
     }
 
-    // Main function to add two numbers represented as linked lists.
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* r1 = nullptr;
-        ListNode* r2 = nullptr;
-
-        // Reverse both linked lists to simplify addition.
-        int len1 = reverse(l1, r1);
-        int len2 = reverse(l2, r2);
-
-        // Ensure 'r1' represents the longer number and 'r2' represents the shorter number.
-        if (len1 < len2) {
-            swap(len1, len2);
-            swap(r1, r2);
-        }
-
+        // Reverse both input linked lists
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        
+        ListNode* result = new ListNode(0); // Dummy node to start the result list
+        ListNode* ptr = result;
         int carry = 0;
-        ListNode* ptr1 = r1;
-        ListNode* ptr2 = r2;
-        ListNode* ans = NULL;
-        ListNode* next = NULL;
 
-        // Iterate through the longer list and add corresponding digits from the shorter list.
-        while (ptr1) {
-            if (ptr2) {
-                ptr1->val += ptr2->val + carry;
-                ptr2 = ptr2->next;
-            } 
-            else {
-                ptr1->val += carry;
+        // Add the two numbers
+        while (l1 != nullptr || l2 != nullptr ) {
+            int sum = carry;
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
             }
-
-            // Update carry and calculate new value for the current digit.
-            carry = ptr1->val / 10;
-            ptr1->val %= 10;
-
-            // Reverse the processed node and move to the next node.
-            next = ptr1->next;
-            ptr1->next = ans;
-            ans = ptr1;
-            ptr1 = next;
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            
+            carry = sum / 10;
+            sum = sum % 10;
+            
+            ptr->next = new ListNode(sum);
+            ptr = ptr->next;
         }
 
-        // If there is any carry left after processing all digits, add it as a new node.
-        if (carry == 1) {
-            ListNode* newNode = new ListNode(1);
-            newNode->next = ans;
-            ans = newNode;
-        }
-        return ans;
+        // Reverse the result list to get the final answer
+        result = reverseList(result->next);
+        return result;
     }
 };
